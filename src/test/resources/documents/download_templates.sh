4f59ddbfb906dbcb325f0181363b4a03fb557855
Shane StClair <shane@axiomalaska.com>
2013-06-23 21:40:17 -0700

    Template download scriptfix

--- src/test/resources/documents/download_templates.sh
+++ src/test/resources/documents/download_templates.sh
@@ -3,4 +3,4 @@ BASE_URL=https://ioostech.googlecode.com/svn/trunk/templates/Milestone1.0
 DIR=`dirname $0`
 wget -O $DIR/M1_0_CAPABILITIES.xml $BASE_URL/SOS-GetCapabilities.xml
 wget -O $DIR/M1_0_SENSOR_ML_NETWORK.xml $BASE_URL/SML-DescribeSensor-Network.xml
-wget -O $DIR/M1_0_SENSOR_ML_STATION $BASE_URL/SML-DescribeSensor-Station.xml
+wget -O $DIR/M1_0_SENSOR_ML_STATION.xml $BASE_URL/SML-DescribeSensor-Station.xml
